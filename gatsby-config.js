1600cb384b30db9f191c3eb04d1c2cd2177c3780
norellanac <1005alexis@gmail.com>
2021-11-23 18:34:54 -0600

    finalizando con imagenes en gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,8 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "My Website",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
