51b1d846fa1880a0c33f6205e1c7dcd758ecf918
norellanac <1005alexis@gmail.com>
2021-11-26 23:43:07 -0600

    Updates

--- component---src-pages-about-js-39ac62645bc83db07de5.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[682],{5518:function(e,t,a){a.r(t);var s=a(7294),u=a(8553),l=a(6325),n=a(4381);t.default=function(e){return s.createElement(u.Z,e,s.createElement("h1",null,s.createElement(l.Z,{id:"about-us",defaultMessage:"About us"})),s.createElement(n.Z,{className:"btn btn-link",href:"/"},s.createElement(l.Z,{id:"homepage",defaultMessage:"Homepage"})))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-39ac62645bc83db07de5.js.map
